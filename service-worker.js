0781c20947a00546a559377736709b4727464e98
mereslith <l9000003216@gmail.com>
2025-09-01 09:30:51 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,15 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('tax-refund-cache').then(function(cache) {
-            return cache.addAll([
-                './index.html',
-                './manifest.json',
-                './icon-192.png',
-                './icon-512.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('tax-refund-cache-v7').then(function(cache) {
+      return cache.addAll(['./index.html','./manifest.json','./icon-192.png','./icon-512.png']);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
